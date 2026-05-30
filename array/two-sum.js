80f5df215d0ddac17843fd590aad8477886f3b47
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-05-30 11:16:27 +0900

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.js
+++ array/two-sum.js
@@ -4,11 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (let i=0;i<nums.length;i++){
-        for (let j=i+1; j<nums.length; j++){
-            if (nums[i] + nums[j] === target){
-                return [i,j];
-            }
+    hashMap = {};
+
+    for (let i = 0; i< nums.length; i++){
+        let diff = target - nums[i];
+        if (diff in hashMap){
+            return [hashMap[diff], i];
+        }
+        else{
+            hashMap[nums[i]] = i;
         }
     }
 };
