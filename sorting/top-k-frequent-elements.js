dd142568bfd86ee219fac14514e8c8e27ccb9cf7
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-04 10:36:38 +0900

    LC-top-k-frequent-elements [Runtime: N/A; Memory: N/A]

--- sorting/top-k-frequent-elements.js
+++ sorting/top-k-frequent-elements.js
@@ -15,9 +15,5 @@ var topKFrequent = function(nums, k) {
     }
 
     let sortedArray = Object.entries(map).sort((a, b) => b[1] - a[1] );
-    let result = [];
-    for (let i = 0; i < k; i++){
-        result.push(parseInt(sortedArray[i][0]));
-    }
-    return result;
+    return sortedArray.slice(0, k).map(item => parseInt(item[0]));
 };
