21dcb0473c6fe53b6742fb89bf184578d8b4c550
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-05 16:58:11 +0900

    LC-missing-number [Runtime: N/A; Memory: N/A]

---
+++ math/missing-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    for (let i = 0; i < nums.length + 1; i++){
+        console.log(i);
+        if (!nums.includes(i)){
+            return i;
+        }
+    }
+    return 0;
+
+};
