637f2d8ac6506d01ab8e4dc03c7f31e498e52266
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-07 09:21:11 +0900

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.js
+++ union-find/longest-consecutive-sequence.js
@@ -6,17 +6,19 @@ var longestConsecutive = function(nums) {
     let set = new Set(nums);
     let longest = 0;
 
-    for (let num of nums){
-        if (set.has(num-1)) continue;
-        
-        let currentNum = num;
-        let current = 1;
-        while (set.has(currentNum+1)){
-            current++;
+    for (let num of nums) {
+        if (set.has(num - 1)) continue;
+
+        let currentNum = num; 
+        let currentStreak = 1;
+
+        while (set.has(currentNum + 1)) {
             currentNum++;
+            currentStreak++;
         }
 
-        longest = Math.max(current, longest);
+        longest = Math.max(longest, currentStreak);
     }
+
     return longest;
 };
