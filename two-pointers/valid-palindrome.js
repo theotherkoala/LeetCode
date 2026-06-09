d85e51a9f41bc5b5ba1088b232986fb2b02b52bf
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-10 08:25:41 +0900

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/valid-palindrome.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    const cleanedString = s.replace(/[^a-zA-Z0-8]/g, '').toLowerCase();
+    let left = 0;
+    let right = cleanedString.length - 1;
+    while (left < right){
+        console.log(cleanedString[left]);
+        console.log(cleanedString[right]);
+        if (cleanedString[left] !== cleanedString[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
