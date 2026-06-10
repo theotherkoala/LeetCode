3b77c3d6d185dd0a1e086a80ed4a4325bf97401d
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-10 09:01:14 +0900

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- two-pointers/valid-palindrome.js
+++ two-pointers/valid-palindrome.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const cleanedString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const cleanedString = cleanUp(s);
     let left = 0;
     let right = cleanedString.length - 1;
     while (left < right){
@@ -15,3 +15,16 @@ var isPalindrome = function(s) {
     }
     return true;
 };
+
+function cleanUp (s) {
+    const valid = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    let result = ""
+
+    for (let i = 0; i < s.length; i++){
+        let eachChar = s[i].toLowerCase();
+        if (valid.indexOf(eachChar) !== -1 ){
+            result+= eachChar;
+        }
+    }
+    return result;
+}
