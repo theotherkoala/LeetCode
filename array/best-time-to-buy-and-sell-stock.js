a461eabad89732d1434e21311eeb56e92bd1be73
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-12 08:56:22 +0900

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

---
+++ array/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let curMin = prices[0];
+    let curMax = 0;
+     // 7 0, 7 0 
+    for (let i = 0; i < prices.length; i++){
+        curMin = Math.min(prices[i], curMin);
+        curMax = Math.max(curMax, prices[i] - curMin);
+        console.log(curMin);
+        console.log(curMax);
+    }
+    return curMax;
+    
+};
