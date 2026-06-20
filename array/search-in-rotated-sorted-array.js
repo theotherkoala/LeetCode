7b7488e0afd2dd6ee0c51f95eb0796161aa9c44c
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-20 10:48:24 +0900

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ array/search-in-rotated-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right){
+        mid = Math.floor((left + right)/2);
+        if (nums[mid] === target){
+            return mid;
+        }
+
+        if (nums[mid] > nums[left]){
+            if (target < nums[left] || target > nums[mid]){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }else {
+            if (target > nums[right] || target < nums[mid]){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+    }
+    return -1;
+
+};
