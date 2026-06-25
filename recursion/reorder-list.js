5c77c5302986585c059ca20d15423c8f21c2881f
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-25 15:35:20 +0900

    LC-reorder-list [Runtime: N/A; Memory: N/A]

---
+++ recursion/reorder-list.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    let curr = slow.next; 
+    slow.next = null; 
+
+    let prev = null;
+    while (curr){
+        let temp = curr.next;
+        curr.next = prev;
+        prev= curr;
+        curr = temp;       
+    }
+    console.log(prev);
+
+    let first = head;
+    let second = prev;
+    
+    while (second) {
+        let temp1 = first.next;
+        let temp2 = second.next;
+
+        first.next = second;  // 1 -> 4
+        second.next = temp1;  // 4 -> 2
+
+        first = temp1;
+        second = temp2;
+    }
+
+
+};
