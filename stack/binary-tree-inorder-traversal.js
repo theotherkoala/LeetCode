e0ed4b63dc3aa42c014919e9838c32836b82e81c
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-28 20:14:51 +0900

    LC-binary-tree-inorder-traversal [Runtime: N/A; Memory: N/A]

--- stack/binary-tree-inorder-traversal.js
+++ stack/binary-tree-inorder-traversal.js
@@ -11,19 +11,33 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    let res = [];
-    let stack = [];
-    let cur = root;
-    while (cur || stack.length > 0){
-        while (cur){
-            stack.push(cur);
-            cur = cur.left;
-        }
-        cur = stack.pop();
-        res.push(cur.val);
-        cur = cur.right;
-    }
+    // let res = [];
+    // let stack = [];
+    // let cur = root;
+    // while ( stack.length > 0){
+    //     while (cur){
+    //         stack.push(cur);
+    //         cur = cur.left;
+    //     }
+    //     cur = stack.pop();
+    //     res.push(cur.val);
+    //     cur = cur.right;
+    // }
 
+    // return res;
+
+    //recursive
+    let res = [];
+
+    function inorder(root){
+        if (!root) return res;
+
+        inorder(root.left);
+        res.push(root.val);
+        inorder(root.right);
+
+    }
+    inorder(root);
     return res;
 
 };
