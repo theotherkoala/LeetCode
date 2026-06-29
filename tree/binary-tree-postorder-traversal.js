c903c457060740528f2736fc0d50ab26c9bd2afd
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-06-29 15:06:49 +0900

    LC-binary-tree-postorder-traversal [Runtime: N/A; Memory: N/A]

---
+++ tree/binary-tree-postorder-traversal.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    let res = [];
+
+    function postorder(root){
+        if (!root) return res;
+        postorder(root.left);
+        postorder(root.right);
+        res.push(root.val);
+    }
+
+    postorder(root);
+    return res;
+};
