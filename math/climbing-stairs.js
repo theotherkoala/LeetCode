0320c500993cadb0b1c1933ed04918bcc60e87f9
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-07-05 21:57:15 +0900

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- math/climbing-stairs.js
+++ math/climbing-stairs.js
@@ -3,6 +3,16 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    if (n <= 2) return n;
-    return climbStairs(n - 1) + climbStairs(n - 2);
+if (n <= 2) return n;
+    
+    let first = 1;
+    let second = 2;
+    
+    for (let i = 3; i <= n; i++) {
+        const third = first + second;
+        first = second;
+        second = third;
+    }
+    
+    return second;
 };
