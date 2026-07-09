35b96f92dece004cca8ae065639b5c4dfc2e3cf8
theotherkoala <107599103+theotherkoala@users.noreply.github.com>
2026-07-09 09:23:01 +0900

    LC-subarray-product-less-than-k [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/subarray-product-less-than-k.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function(nums, k) {
+    if (k < 1) return 0;
+    let left = 0;
+    let res = 0;
+    let prod = 1;
+
+    for (let i = 0; i < nums.length; i++){
+        prod *= nums[i];
+        while (prod > k]{
+            prod /= nums[left];
+            left++;
+        })
+        res += i - left + 1;
+    }
+    return res;
+};
